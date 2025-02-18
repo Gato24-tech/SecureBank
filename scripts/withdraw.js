29d39babd5476d37a4cb2a3e08e8d555e4f0f10a
Gato24-tech <ivan.queralt@gmail.com>
2025-02-18 11:14:25 +0100

    Corrección de withdraw

--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -1,16 +1,26 @@
-import hardhat from hardhat;
+import hardhat from "hardhat";
 const { ethers } = hardhat;
 
+async function main() {
 const [owner] = await ethers.getSigners();
-console.log = ("Owner Address.",owner.address);
+console.log("Owner Address:",owner.address);
 
-const contract = await getContractAt("SecureBank","0x5FbDB2315678afecb367f032d93F642f64180aa3");
+const contract = await ethers.getContractAt("SecureBank", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
 
-const balance = await contract.getBalance();
+
+let balance = await contract.getBalance();
 console.log("Saldo actual:", ethers.formatEther (balance), "ETH");
 
 const withdrawAmount = ethers.parseEther("0.5");
-console.log('\Intentando retirar ${ethers.formatEther(withdrawAmount)} ETH...');
+console.log('Intentando retirar ${ethers.formatEther(withdrawAmount)} ETH...');
 
 balance = await contract.getBalance();
-console-log('Saldo después de retiro: "{ethers.formatEther(withdrawAmount)} ETH...');
+console.log('Saldo después de retiro: "{ethers.formatEther(withdrawAmount)} ETH...');
+
+}
+
+// Ejecutar script
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
