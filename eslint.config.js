ff214fdb30d2b95640fdd39792703b7cf52d2c7f
Gato24-tech <ivan.queralt@gmail.com>
2025-02-19 11:25:45 +0100

    cambio en eslint.confing.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node, // Cambiado de browser a node
+    },
+    rules: {
+      "no-unused-vars": "warn", // Advertencia para variables no usadas
+      "no-console": "off", // Permitimos console.log()
+      "semi": ["error", "always"], // Obligamos a usar punto y coma
+    },
+  },
   pluginJs.configs.recommended,
 ];
