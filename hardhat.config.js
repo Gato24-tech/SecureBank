92eda598bef31d4a3bebe219a313e6ccffb34a6a
Gato24-tech <ivan.queralt@gmail.com>
2025-02-25 10:25:53 +0100

    modificacion archivo hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-require("@nomicfoundation/hardhat-toolbox");
+import "dotenv/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { defineConfig } from "hardhat/config";
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
 
-module.exports = {
+export default defineConfig({
   solidity: "0.8.28",
   networks: {
-      localhost: {
+    localhost: {
       url: "http://127.0.0.1:8545",
-      hardhat: {}
+      hardhat: {},
     },
-
     arbitrumSepolia: {
       url: `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
@@ -22,4 +22,4 @@ module.exports = {
     currency: "USD",
     coinmarketcap: process.env.COINMARKETCAP_API_KEY || "",
   },
-};
+});
